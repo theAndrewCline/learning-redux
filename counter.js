5f6e83b8005d3feeb1fd3949ee564cf0a7b6d22e
theAndrewCline <andrew.cline77@gmail.com>
2018-03-08 20:26:20 -0600

    removed webpack

---
+++ counter.js
@@ -0,0 +1,16 @@
+const Redux = require('Redux')
+const counter = (state = 0, action) => {
+	switch (action.type) {
+		case 'INCREMENT':
+			return state + 1
+		case 'DECREMENT':
+			return state - 1
+		default:
+			return state
+	}
+}
+
+const { createStore } = Redux
+const store = createStore(counter)
+
+module.exports = counter
--- src/index.js
+++
@@ -1,8 +0,0 @@
-const root = document.querySelector('#root')
-
-console.log(root)
-
-const name = 'Andrew'
-const string = `Hello World, My Name is ${name}`
-
-root.innerHTML(string)
