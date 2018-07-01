c200dd57928620f670582a75eb748061032cca76
theandrewcline <andrew.cline77@gmail.com>
2018-07-01 17:54:25 -0500

    adding React, creating store manually

--- excercises/js/counter.js
+++ excercises/js/counter.js
@@ -1,12 +1,50 @@
-module.exports = function counter(state, action) {
-    if (typeof state === 'undefined') {
-        return 0
-    }
-    if (action.type === 'INCREMENT') {
-        return state + 1
-    } else if (action.type === 'DECREMENT') {
-        return state - 1
-    } else {
-        return state
-    }
+function counter(state, action) {
+   if (typeof state === 'undefined') {
+      return 0
+   }
+   if (action.type === 'INCREMENT') {
+      return state + 1
+   } else if (action.type === 'DECREMENT') {
+      return state - 1
+   } else {
+      return state
+   }
 }
+
+const Counter = ({ value, onIncrement, onDecrement }) => (
+   <div>
+      <h1>{value}</h1>
+      <button onclick={onIncrement}>+</button>
+      <button onclick={onDecrement}>-</button>
+   </div>
+)
+
+const { createStore } = Redux
+
+const store = createStore(counter)
+
+const render = () => {
+   ReactDOM.render(
+      <Counter
+         value={store.getState()}
+         onIncrement={() => {
+            store.dispatch({
+               type: 'INCREMENT'
+            })
+         }}
+         onDecrement={() => {
+            store.dispatch({
+               type: 'DECREMENT'
+            })
+         }}
+      />,
+      document.getElementById('root')
+   )
+}
+
+store.subscribe(render)
+render()
+
+document.addEventListener('click', () => {
+   store.dispatch({ type: 'INCREMENT' })
+})
---
+++ notes/reduxAction.md
@@ -0,0 +1,11 @@
+# Redux Actions 
+
+The store combines the 3 principles of redux. The store is going to hold the state of the application. 
+
+### getState() 
+
+Returns the current state of the Redux store
+
+### dispatch() 
+
+### subscribe()
